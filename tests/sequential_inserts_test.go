225a2a91e302dbb2a866b6b65941591f5e65507c
owengombas <public.owen@gombas.ch>
2024-05-23 16:35:53 +0200

    Tests

--- tests/sequential_inserts_test.go
+++ tests/sequential_inserts_test.go
@@ -1,57 +1,23 @@
 package tests
 
 import (
+	"fmt"
 	"testing"
 )
 
-func TestSequentialInserts1k(t *testing.T) {
-	keys := GenerateRandomKeys(1_000)
-	SaveKeysToCSV(keys)
-	alex, keys, err := SequentialInserts(keys)
-	if err != nil {
-		t.Error(err)
-	}
-	err = SequentialLookups(alex, keys)
-	if err != nil {
-		t.Error(err)
-	}
-}
-
-func TestSequentialInserts10k(t *testing.T) {
-	keys := GenerateRandomKeys(10_000)
-	SaveKeysToCSV(keys)
-	alex, keys, err := SequentialInserts(keys)
-	if err != nil {
-		t.Error(err)
-	}
-	err = SequentialLookups(alex, keys)
-	if err != nil {
-		t.Error(err)
-	}
-}
-
-func TestSequentialInserts100k(t *testing.T) {
-	keys := GenerateRandomKeys(100_000)
-	SaveKeysToCSV(keys)
-	alex, keys, err := SequentialInserts(keys)
-	if err != nil {
-		t.Error(err)
-	}
-	err = SequentialLookups(alex, keys)
-	if err != nil {
-		t.Error(err)
-	}
-}
-
-func TestSequentialInserts1m(t *testing.T) {
-	keys := GenerateRandomKeys(1_000_000)
-	SaveKeysToCSV(keys)
-	alex, keys, err := SequentialInserts(keys)
-	if err != nil {
-		t.Error(err)
-	}
-	err = SequentialLookups(alex, keys)
-	if err != nil {
-		t.Error(err)
+func TestSequentialInserts1kto1m(t *testing.T) {
+	for i := 1_000; i <= 10_000_000; i *= 10 {
+		t.Run(fmt.Sprintf("SequentialInserts%d", i), func(t *testing.T) {
+			keys := GenerateRandomKeys(i)
+			SaveKeysToCSV(keys)
+			alex, keys, err := SequentialInserts(keys)
+			if err != nil {
+				t.Error(err)
+			}
+			err = SequentialLookups(alex, keys)
+			if err != nil {
+				t.Error(err)
+			}
+		})
 	}
 }
